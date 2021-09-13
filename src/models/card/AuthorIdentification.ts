5ce4faa6758bce603efa5342d5334dd2186dd8f3
Ferran Llop <ferranllop@gmail.com>
2021-09-13 18:16:14 +0200

    Remove AuthorIdentification unnecesasary constructor

--- src/models/card/AuthorIdentification.ts
+++ src/models/card/AuthorIdentification.ts
@@ -2,11 +2,8 @@ import { UserIdentification } from '../user/UserIdentification.js'
 
 export class AuthorIdentification extends UserIdentification {
 
-    constructor(id: string) {
-        super(id)
-    }
-
     clone(): AuthorIdentification {
         return new AuthorIdentification(this.getId())
     }
+    
 }
