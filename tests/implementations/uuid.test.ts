744390b651ac015156d2e837abc4f0ba3e0610ea
Ferran Llop <contacto@ferranllop.com>
2022-01-10 16:18:47 +0100

    Remove unnecessary test

--- tests/implementations/uuid.test.ts
+++ tests/implementations/uuid.test.ts
@@ -9,12 +9,4 @@ nodeNativeUuid('should be the active implementation', () => {
     assert.equal(ImplementationsContainer.get(Dependency.UUID), new NodeNativeUuid())
 })
 
-nodeNativeUuid.skip('should almost always provide unique identifiers', () => {
-    const generator = new NodeNativeUuid()
-    const uuid = generator.getUuid()
-    for (let i = 0; i < 1_000_000; i++){
-        assert.not.equal(generator.getUuid(), uuid)
-    }
-})
-
 nodeNativeUuid.run()
