76906788cee1074add3524a261066b96bec8a658
Ferran Llop <contacto@ferranllop.com>
2022-01-21 12:55:03 +0100

    Remove unused Mother interface

--- tests/domain/shared/Mother.ts
+++
@@ -1,8 +0,0 @@
-import { IdDto } from '../../../src/domain/shared/value/IdDto.js';
-
-
-export interface Mother<T extends IdDto> {
-    TABLE_NAME: string;
-    numberedDto(number: number): T;
-    dto(): T;
-}
