914769e1845335ecab4da3e0489cbb204482967e
Ferran Llop <contacto@ferranllop.com>
2022-03-25 16:41:06 +0100

    Add test to make explicit that the use cases that requires authorization return a UserIsNotAuthorized when the requester does not exist

--- tests/use-cases/WithAuthorizationUseCase.test.ts
+++ tests/use-cases/WithAuthorizationUseCase.test.ts
@@ -3,19 +3,47 @@ import {Response} from '../../src/use-cases/Response.js'
 import {RequiredRequestFieldIsMissingError} from '../../src/domain/errors/RequiredRequestFieldIsMissingError.js'
 import {WithAuthorizationUseCase, WithRequesterRequest} from '../../src/index.js'
 import {InputDataNotValidError} from '../../src/domain/errors/InputDataNotValidError.js'
+import {RequesterIdentification} from '../../src/domain/authorization/permission/RequesterIdentification.js'
+import {UserIsNotAuthorizedError} from '../../src/domain/errors/UserIsNotAuthorizedError.js'
+import {Authorization} from '../../src/domain/authorization/Authorization.js'
+import {PermissionValidator} from '../../src/domain/authorization/permission/PermissionValidator.js'
+import {UserPermissions} from '../../src/domain/authorization/UserPermissions.js'
+import {PermissionValue} from '../../src/domain/authorization/permission/PermissionValue.js'
+import {
+    givenACleanInMemoryDatabase,
+    givenAStoredUser,
+} from '../implementations/persistence/in-memory/InMemoryDatastoreScenarios.js'
+
+class FailingPermission implements PermissionValidator<null> {
+    validate(): PermissionValue[] {
+        return ['CREATE_OWN_CARD'];
+    }
+}
 
 class TestableUseCase extends WithAuthorizationUseCase<WithRequesterRequest, null> {
+    private Permission?: new (permissions: UserPermissions) => PermissionValidator<unknown>
+
     constructor() {
         super(['fieldA'])
     }
 
     async internalExecute(): Promise<Response<null>> {
-        await this.getRequesterPermissions()
+        const permissions = await this.getRequesterPermissions()
+        if(this.Permission){
+            Authorization.assertUserWithPermissions(permissions).can(this.Permission)
+        }
         return Response.OkWithoutData()
+    }
+
+    withPermission(permission: new (permission: UserPermissions) => PermissionValidator<unknown>){
+        this.Permission = permission
+        return this
     }
 }
 
 const useCase = suite('With authorization use Case')
+
+useCase.before.each(async () => await givenACleanInMemoryDatabase())
 
 useCase('should return whatever the use case return' +
     'when the required fields are present in request', async () => {
@@ -51,13 +79,41 @@ useCase(
             fieldA: 'someData',
             fieldB: 'someData',
         }
-        try {
         const result = await new TestableUseCase().execute(invalidRequesterRequest)
         assert.equal(result, Response.withDomainError(new InputDataNotValidError()))
+    })
 
-        } catch (error) {
-            console.log(error)
+useCase(
+    'given a nonexistent requesterId, ' +
+    'should return an object with null as data property and ' +
+    'UserIsNotAuthorizedError DomainError', async () => {
+        await givenAStoredUser()
+        const nonexistentRequesterId = RequesterIdentification.create().getId()
+        const invalidRequesterRequest = {
+            requesterId: nonexistentRequesterId,
+            fieldA: 'someData',
+            fieldB: 'someData',
         }
+        const result = await new TestableUseCase()
+            .withPermission(FailingPermission)
+            .execute(invalidRequesterRequest)
+        assert.equal(result, Response.withDomainError(new UserIsNotAuthorizedError(['CREATE_OWN_CARD'])))
+    })
+
+useCase(
+    'given a nonexistent users table, ' +
+    'should return an object with null as data property and ' +
+    'UserIsNotAuthorizedError DomainError', async () => {
+        const nonexistentRequesterId = RequesterIdentification.create().getId()
+        const invalidRequesterRequest = {
+            requesterId: nonexistentRequesterId,
+            fieldA: 'someData',
+            fieldB: 'someData',
+        }
+        const result = await new TestableUseCase()
+            .withPermission(FailingPermission)
+            .execute(invalidRequesterRequest)
+        assert.equal(result, Response.withDomainError(new UserIsNotAuthorizedError(['CREATE_OWN_CARD'])))
     })
 
 
